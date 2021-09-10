26d68065f33235f29f6f504558f5c23f3dc0b7d6
ssfructuoso <salvadorsantiago.fructuoso@gmail.com>
2021-09-10 03:53:08 +0200

    Comentado scripts de graficado de las simulaciones

--- plot_graficas/M1.py
+++ plot_graficas/M1.py
@@ -1,32 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas
-import math
 import scipy.integrate as integrar
 import tailer as tl
-import matplotlib.ticker
-from matplotlib.ticker import FormatStrFormatter
-from scipy.interpolate import interp1d
-from decimal import Decimal
-from scipy.interpolate import CubicSpline
-
-from scipy.stats import norm
-
 import io
-from matplotlib import colors
 from scipy.interpolate import CubicSpline
 
 
+###########################################
+### Script utilizado para calcular el primer momento M1(t) con los dos metodos numericos desarrollados.
+### En este caso se ha escogido el nucleo producto, pudiendose diferenciar el fenomeno de gelacion.
+### Este script es utilizado para generar la figura 2.10 de la memoria del trabajo.
+###########################################
 
 
-# Documento txt a representar:
-# datosTxt='volumentesFin.txt'
-
-
-
-def plot1(datosTxt, seccional, pltlabel):
+def plot1(datosTxt, pltlabel):
     
-
     dfHeader = pandas.read_csv(datosTxt, sep=';', delimiter=';', nrows=1, names=["0", "1", "2", "3", "4", "5", "6"])
 
     iniX12 = dfHeader.iloc[0, 1]
@@ -38,14 +27,12 @@ def plot1(datosTxt, seccional, pltlabel):
 
     xi12 = dfX12.iloc[0, :]
     xi12 = xi12.to_numpy()
-    # print(xi12)
 
     xi = np.zeros(len(xi12) - 1)
     for i in range(0, len(xi)):
         xi[i]=0.5*(xi12[i]+xi12[i+1])
 
     g = g.to_numpy()
-    # g=np.delete(g,0,axis=0)
 
     ##############
     file1 = open(datosTxt)
@@ -56,11 +43,7 @@ def plot1(datosTxt, seccional, pltlabel):
     tiempo = df1.iloc[1]
     tiempo = tiempo.to_numpy()
 
-    # print(tiempo)
-
     #############
-
-
     def m1(posicion):
         x=np.append(xi12[0],xi)
         x=np.append(x,xi12[len(xi12)-1])
@@ -75,23 +58,17 @@ def plot1(datosTxt, seccional, pltlabel):
         integral_f = integrar.simps(f(x_simp), x_simp)
         return integral_f
 
-
-
     fTiempo = np.zeros(len(tiempo))
     for i in range(0, len(tiempo)):
         fTiempo[i] = m1(i)
-    if seccional==True: fTiempo[0]=fTiempo[1]
 
     plt.figure(1)
     plt.plot(tiempo, fTiempo, label=pltlabel)
     
-    ###
-    
-    
-    print('plot1:', np.abs(fTiempo[0] - fTiempo[len(tiempo) - 1]))
+######################
+######################
 
-######################
-######################
+#### Momento M1 exacto para el nucleo producto (Ker(x,y)=xy)
 tExacta=np.linspace(0.001,2,num=10000)
 m1Exacta=[]
 for i in tExacta:
@@ -100,22 +77,22 @@ for i in tExacta:
     else:
         m1Exacta.append(i**(-0.5))
 
+
 plt.plot(tExacta,m1Exacta, 'k', label='Sol. exacta')###
 plt.title('M1 (Ker(x,y)=xy)')
 plt.xlabel('t')
 plt.ylabel('M$_{1}$(t)')
 plt.xlim(0,1.6)
 plt.ylim(0.6,1.2)
-datosVol= 'M1/PROD/vol_finitos_RK3_PROD_trapecios_compuesto_100.txt'
-datosSec='M1/PROD/SECC-PROD-finish_100.txt'
-plot1(datosVol, False, "Vol. finitos")
-plot1(datosSec, False, "Seccional")
+
+
+datosVol= 'ficheros_simulaciones/M1/Ker_producto/vol_finitos_RK3_PROD_trapecios_compuesto_100.txt'
+datosSec='ficheros_simulaciones/M1/Ker_producto/secc_RK3_PROD_trapecios_compuesto_100.txt'
+plot1(datosVol, "Vol. finitos") #Momento M1 para el nucleo producto con el metodo de volumenes finitos
+plot1(datosSec, "Seccional")#Momento M1 para el nucleo producto con el metodo seccional
 xV=[1,1]
 yV=[-1,2]
 plt.plot(xV,yV,'r--')
-#plot3()
-#plot4()
-#plot5()
 plt.legend()
 #plt.gca().xaxis.set_major_formatter(Formatter(fformat="%1.1f"))
 plt.xlim(0,2.5)
